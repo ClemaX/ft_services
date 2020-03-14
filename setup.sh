7ce65352ebcc89097c23797ca6a0c8abf1cc5087
Clément Hamada <chamada@student.le-101.fr>
2020-03-14 14:54:06 +0100

    W.I.P.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DRIVER=virtualbox
+PREFIX=ft_services
+SRCDIR=srcs
+
+# Start minikube
+minikube start --driver=$DRIVER
+
+# Use minikube docker-env
+eval $(minikube docker-env)
+
+# Build docker images
+docker build -t $PREFIX/mysql $SRCDIR/mysql
+docker build -t $PREFIX/wordpress $SRCDIR/wordpress
+docker build -t $PREFIX/wordpress $SRCDIR/wordpress
+
+# Apply kustomization
+kubectl apply -k $SRCDIR
