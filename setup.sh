56dd935769cabbdcd18aaaa8ad3f68de6b2a8496
Clément Hamada <chamada@student.le-101.fr>
2020-03-17 20:01:10 +0100

    Enable web dashboard

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ SRCDIR=srcs
 # Start minikube
 minikube start --driver=$DRIVER
 
-# Enable ingress
+# Enable addons
+minikube addons enable metrics-server
+minikube addons enable dashboard
 minikube addons enable ingress
 
 # Use minikube docker-env
@@ -19,3 +21,6 @@ docker build -t $PREFIX/wordpress $SRCDIR/wordpress
 
 # Apply kustomization
 kubectl apply -k $SRCDIR
+
+# Show web dashboard url
+minikube dashboard --url
