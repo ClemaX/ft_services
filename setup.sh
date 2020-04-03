0c25846a26d40fe3c4d5ccc555b6ad38c2fef549
Clément Hamada <clementhamada@protonmail.com>
2020-04-03 15:19:24 +0200

    ingress: Use nginx as backend and generate certs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-DRIVER=kvm2
-PREFIX=ft_services
-SRCDIR=srcs
+DRIVER=kvm2         # Driver to use with minikube
+PREFIX=ft_services  # Docker build prefix
+SRCDIR=srcs         # Directory which contains the deployments
+
+KEYDIR=keys         # Directory where keys and certs will be generated
+KEYHOST=ft.services # Load balancer hostname
 
 # Start minikube
 minikube start --driver=${DRIVER}
@@ -19,7 +22,20 @@ eval $(minikube docker-env)
 docker build -t ${PREFIX}/mysql ${SRCDIR}/mysql
 docker build -t ${PREFIX}/wordpress ${SRCDIR}/wordpress
 docker build -t ${PREFIX}/phpmyadmin ${SRCDIR}/phpmyadmin
+docker build -t ${PREFIX}/nginx ${SRCDIR}/nginx
 docker build -t ${PREFIX}/ftps ${SRCDIR}/ftps
+
+# Generate TLS keys for ingress
+mkdir -p ${KEYDIR}
+
+openssl req -x509 -nodes -days 365\
+    -newkey rsa:2048\
+    -keyout ${KEYDIR}/${KEYHOST}.key\
+    -out ${KEYDIR}/${KEYHOST}.csr\
+    -subj "/CN=${KEYHOST}/O=${KEYHOST}"
+
+kubectl delete secret ${KEYHOST}-tls
+kubectl create secret tls ${KEYHOST}-tls --key ${KEYDIR}/${KEYHOST}.key --cert ${KEYDIR}/${KEYHOST}.csr
 
 # Apply kustomization
 kubectl apply -k ${SRCDIR}
